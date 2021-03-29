86173bfc428f65eff69ee45dc741c81395de78ad
Kexicake <slava.kuznetsov.2017@gmail.com>
2021-03-29 20:12:01 +0400

    AAA

---
+++ Assets/Scripts/JumpController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpController : MonoBehaviour
+{
+    [Header("Характеристики прыжка")]
+    [SerializeField]
+    private float jumpForce = 200f;
+    [Range(0.002f, 1f)]
+    [SerializeField]
+    private float activeJumpTime = 0.15f;
+
+    [Header("Имя параметра прыжка в аниматоре")]
+    [SerializeField]
+    private string groundParameterName = "isGrounded";
+
+    private CapsuleCollider2D cc2d;
+    private Rigidbody2D rb2d;
+    private Animator animator;
+
+    private bool onGround = false;
+
+    private void Awake()
+    {
+        cc2d = GetComponent<CapsuleCollider2D>();
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate ()
+    {
+        CheckIsGrounded();
+        
+        if (animator != null)
+            animator.SetBool(groundParameterName, onGround);
+
+        bool canJump = Input.GetKey(KeyCode.Space) && onGround && rb2d != null;
+
+        if (canJump)
+        {
+            StopAllCoroutines();
+            StartCoroutine(JumpCoroutine());
+        }
+    }
+
+    private void CheckIsGrounded()
+    {
+        cc2d.enabled = false;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + Vector3.down * (cc2d.size.y / 2 - 0.1f), Vector3.down, cc2d.size.x / 4);
+        cc2d.enabled = true;
+
+        onGround = hit.collider && !hit.collider.isTrigger;
+    }
+
+    private IEnumerator JumpCoroutine()
+    {
+        rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
+        
+        float currentJumpTime = 0f;
+        while (Input.GetKey(KeyCode.Space) && currentJumpTime < activeJumpTime)
+        {
+            float timeMultiplier = 1f - currentJumpTime / activeJumpTime;
+            rb2d.AddForce(Vector2.up * jumpForce * timeMultiplier * Time.fixedDeltaTime, ForceMode2D.Impulse);
+            currentJumpTime += Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour
+{
+    [Header("Скорость передвижения")]
+    public float speed = 7f;
+
+    [Header("Имя параметра передвижения в аниматоре")]
+    public string walkParameterName = "isWalk";
+
+    [Header("Первое положение")]
+    public float firstPlat = -0.588f;
+
+    [Header("Второе положение")]
+    public float secondPlat = 1.48f;
+
+    [Header("Третье положение")]
+    public float therdPlat = 3.45f;
+
+
+    [Header("Шакал ебучий")]
+    public float dPlat = 0f;
+
+    private SpriteRenderer spriteRenderer;
+    private CapsuleCollider2D cc2d;
+    private Rigidbody2D rb2d;
+    private Animator animator;
+
+    private bool isCanMove = true;
+    private bool isFirst = true;
+    private bool isSecond = false;
+    private bool isTherd = false;
+    private bool isStay = true;
+    private bool isTurnRight = true;
+
+    private void Awake ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        cc2d = GetComponent<CapsuleCollider2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
+    private void FixedUpdate()
+    {
+CheckIsCanMove();
+    
+        float movement = Input.GetAxis("Vertical");
+
+        isStay = Mathf. Abs(movement) <= 0.15f;
+
+        if (movement > 0)
+            isTurnRight = true;
+        else if (movement < 0)
+            isTurnRight = true;
+
+        spriteRenderer.flipX = !isTurnRight;
+
+        if (animator != null)
+            animator.SetBool(walkParameterName, !isStay);
+
+        if (rb2d != null)
+        {
+            if (isFirst)
+            {
+                if (this.gameObject.transform.position.y >= secondPlat)
+                {
+                    rb2d.velocity = new Vector2(rb2d.velocity.x, 0f);
+                }
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    print(this.gameObject.transform.position.y);
+
+                    if (this.gameObject.transform.position.y >= secondPlat)
+                    {
+                        rb2d.velocity = new Vector2(rb2d.velocity.x, 0f);
+                    }
+                    else
+                    {
+                        rb2d.velocity = new Vector2(rb2d.velocity.x, 1 * speed);
+                    }   
+                }
+                if (this.gameObject.transform.position.y >= secondPlat)
+                {
+                    isFirst = false;
+                    isSecond = true;
+                }
+                
+            }else if (isSecond)
+            {
+                if (this.gameObject.transform.position.y >= therdPlat)
+                {
+                    rb2d.velocity = new Vector2(rb2d.velocity.x, 0f);
+                }
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    print(this.gameObject.transform.position.y);
+
+                    if (this.gameObject.transform.position.y >= therdPlat)
+                    {
+                        rb2d.velocity = new Vector2(rb2d.velocity.x, 0f);
+                    }
+                    else
+                    {
+                        rb2d.velocity = new Vector2(rb2d.velocity.x, 1 * speed);
+                    }
+                }
+                if (this.gameObject.transform.position.y >= therdPlat)
+                {
+                    isSecond = false;
+                    isTherd = true;
+                }
+                
+            }
+        }  
+    }
+
+    private void CheckIsCanMove()
+    {
+        cc2d.enabled = false;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, isTurnRight ? Vector3.down : Vector3.up, cc2d.size.y + 0.1f);
+        cc2d.enabled = true;
+
+        isCanMove = !hit.collider || hit.collider.isTrigger;
+    }
+}
